f850606a39740ee13d932a83b0c63bbf3a684216
AssimAouraghe <104429990+AssimAouraghe@users.noreply.github.com>
2022-07-20 10:47:35 +0200

    Update streamlit_app.py
    Von Breakfast zu Rangeegg

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.subheader('Breakfast Menu')
+
+'Omega 3 & Blueberry Oatmeal'
+
+'Kale, Spinach & Rocket Smoothie'
+
+'Hard-Boiled Free-Range Egg'
